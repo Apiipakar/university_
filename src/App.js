7860984218f266a520d9a7d36458967141d0a8cf
Apiipakar <cayuuni37@gmail.com>
2023-06-15 18:06:07 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
 import { Home } from "./Website/Pages/Home";
 
 // main program function
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
     </BrowserRouter>
   );
 };
